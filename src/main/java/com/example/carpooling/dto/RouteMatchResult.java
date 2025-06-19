c69fa549c197786d8cc8cd9b68b0c4ed12e79a77
Codewizard-18 <jaswanthm811@gmail.com>
2025-06-19 16:40:35 +0530

    Minor enchancements.

--- src/main/java/com/example/carpooling/dto/RouteMatchResult.java
+++ src/main/java/com/example/carpooling/dto/RouteMatchResult.java
@@ -1,11 +1,32 @@
 package com.example.carpooling.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
 public class RouteMatchResult {
     private double score;
     private String reason;
+
+    public RouteMatchResult(double score,String reason){
+        this.score=score;
+        this.reason=reason;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 }
