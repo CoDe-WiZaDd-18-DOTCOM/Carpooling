255cad9bf4addba9fdf9afbc0db838fe8263727f
Codewizard-18 <jaswanthm811@gmail.com>
2025-07-24 22:37:02 +0530

    deploy bug fixing

--- src/main/java/com/example/carpooling/CarpoolingApplication.java
+++ src/main/java/com/example/carpooling/CarpoolingApplication.java
@@ -2,7 +2,7 @@ package com.example.carpooling;
 
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -16,19 +16,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CarpoolingApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
-		System.setProperty("MONGO_URI_TEST", dotenv.get("MONGO_URI_TEST"));
-		System.setProperty("SECRET_KEY", dotenv.get("SECRET_KEY"));
-		System.setProperty("EMAIL",dotenv.get("EMAIL"));
-		System.setProperty("PASSWORD",dotenv.get("PASSWORD"));
-		System.setProperty("REDIS_HOST",dotenv.get("REDIS_HOST"));
-		System.setProperty("REDIS_PORT",dotenv.get("REDIS_PORT"));
-		System.setProperty("REDIS_PASSWORD",dotenv.get("REDIS_PASSWORD"));
-		System.setProperty("GOOGLE_CLIENT_ID",dotenv.get("GOOGLE_CLIENT_ID"));
-		System.setProperty("GOOGLE_CLIENT_SECRET",dotenv.get("GOOGLE_CLIENT_SECRET"));
-		System.setProperty("RABBITMQ_URI",dotenv.get("RABBITMQ_URI"));
-		System.setProperty("GEMINI_API_KEY",dotenv.get("GEMINI_API_KEY"));
+//		Dotenv dotenv = Dotenv.configure().load();
+//		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+//		System.setProperty("MONGO_URI_TEST", dotenv.get("MONGO_URI_TEST"));
+//		System.setProperty("SECRET_KEY", dotenv.get("SECRET_KEY"));
+//		System.setProperty("EMAIL",dotenv.get("EMAIL"));
+//		System.setProperty("PASSWORD",dotenv.get("PASSWORD"));
+//		System.setProperty("REDIS_HOST",dotenv.get("REDIS_HOST"));
+//		System.setProperty("REDIS_PORT",dotenv.get("REDIS_PORT"));
+//		System.setProperty("REDIS_PASSWORD",dotenv.get("REDIS_PASSWORD"));
+//		System.setProperty("GOOGLE_CLIENT_ID",dotenv.get("GOOGLE_CLIENT_ID"));
+//		System.setProperty("GOOGLE_CLIENT_SECRET",dotenv.get("GOOGLE_CLIENT_SECRET"));
+//		System.setProperty("RABBITMQ_URI",dotenv.get("RABBITMQ_URI"));
+//		System.setProperty("GEMINI_API_KEY",dotenv.get("GEMINI_API_KEY"));
 
 		SpringApplication.run(CarpoolingApplication.class, args);
 	}
