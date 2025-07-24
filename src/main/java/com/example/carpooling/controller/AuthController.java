e1304f9640f91696ffa970172ff50c2cee7ef73e
Codewizard-18 <jaswanthm811@gmail.com>
2025-07-24 20:33:26 +0530

    bug fixing

--- src/main/java/com/example/carpooling/controller/AuthController.java
+++ src/main/java/com/example/carpooling/controller/AuthController.java
@@ -8,17 +8,23 @@ import com.example.carpooling.services.UserService;
 import com.example.carpooling.utils.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 @Tag(name = "Authentication APIs", description = "Endpoints for user registration and login using JWT.")
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
     @Autowired
     private UserService userService;
 
@@ -79,5 +85,23 @@ public class AuthController {
         authResponse.setRole(user.getRole().name());
 
         return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    @Operation(
+            summary = "Validate JWT",
+            description = "Validates JWT."
+    )
+    @PostMapping("/validate")
+    public ResponseEntity<?> validate(@RequestBody Map<String,String> map) {
+        try {
+            Boolean check=jwtUtil.validateToken(map.get("token"));
+            if(!Boolean.TRUE.equals(check)) {
+                throw new Exception("Token Expired.");
+            }
+            return ResponseEntity.ok("Valid token");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.UNAUTHORIZED);
+        }
     }
 }
